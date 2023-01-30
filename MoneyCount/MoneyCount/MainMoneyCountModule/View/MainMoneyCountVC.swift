248841b47926954850b35e73a6c69546f1aef419
Алексей <CT4TuEI3@MacBook-Pro-Daniil-2.local>
2023-01-30 15:57:32 +0300

    Add TabBar controller

--- MoneyCount/MoneyCount/MainMoneyCountModule/View/MainMoneyCountVC.swift
+++ MoneyCount/MoneyCount/MainMoneyCountModule/View/MainMoneyCountVC.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-final class MainMoneyCountVC: UIViewController {
+final class MainMoneyCountVC: UITabBarController {
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    
+    // MARK: - Private Methods
+    
+    private func setupUI() {
         view.backgroundColor = .green
+        settingsNavigationBar()
+        settingsTabBar()
+    }
+    
+    private func settingsNavigationBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.left"),
                                                            style: .plain,
                                                            target: self,
@@ -21,7 +33,34 @@ final class MainMoneyCountVC: UIViewController {
         
     }
     
+    private func settingsTabBar() {
+        tabBar.backgroundColor = .systemGray4
+        tabBar.barStyle = .default
+        let expensesVC = UIViewController()
+        expensesVC.view.backgroundColor = .systemGray5
+        
+        let item = UITabBarItem(title: "Expenses",
+                                image: UIImage(systemName: "doc.text"),
+                                selectedImage: nil)
+        expensesVC.tabBarItem = item
+        
+        let balancesVC = UIViewController()
+        balancesVC.view.backgroundColor = .systemGray6
+        
+        let itemsec = UITabBarItem(title: "Balances",
+                                   image: UIImage(systemName: "arrow.left.arrow.right"),
+                                   selectedImage: nil)
+        balancesVC.tabBarItem = itemsec
+        
+        setViewControllers([expensesVC, balancesVC], animated: true)
+    }
+    
+    
+    // MARK: - Actions
+    
     @objc func pressedBtn(){
         navigationController?.pushViewController(MainViewController(), animated: true)
     }
 }
+
+
